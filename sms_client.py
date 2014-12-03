a13c01d3eb1f971d4acb9e3267c414d58f90fb76
Sergey Pavlenko <sergeyp@speedflow.com>
2014-12-03 14:43:38 +0200

    refactoring+loseConnection when unbind from server

--- sms_client.py
+++ sms_client.py
@@ -10,32 +10,44 @@ from settings import HOST, PORT, LOGIN, PASSWORD, SMSCOUNT
 
 
 class MyProtocol(Protocol):
+    BINDED = 'binded'
+    UNBINDED = 'unbinded'
+    CONNECTED = 'connected'
+    INIT = 'init'
+
+    def __init__(self):
+        self.state = self.INIT
+        self.submit_sm_resp_count = 0
+        self.submit_sm_count = 0
+        
+        # created schedule task 
+        # will start after success auth
+        self._send_sms = task.LoopingCall(self._submit_sm)
+
     def dataReceived(self, data):
         pdu = self._bin2pdu(data)
-        if str(pdu.commandId) == 'submit_sm_resp':
-            self.SMSSUBMITTED = self.SMSSUBMITTED + 1
-            print 'submit_sm_resp: ', self.SMSSUBMITTED
-            if SMSCOUNT == self.SMSSUBMITTED:
+        if pdu.commandId.key == 'submit_sm_resp':
+            self.submit_sm_resp_count = self.submit_sm_resp_count + 1
+            print 'submit_sm_resp: ', self.submit_sm_resp_count
+            if SMSCOUNT == self.submit_sm_resp_count:
                 self._unbind()
-        elif str(pdu.commandId) == 'bind_transmitter_resp' and str(pdu.status) == 'ESME_ROK':
+        elif pdu.commandId.key == 'bind_transmitter_resp' and pdu.status.key == 'ESME_ROK':
             print 'Auth OK'
-            self.state = 'BINDED'
-            self.SMSSUBMITTED = 0
-            self.SMSSENDED = 0
-            self.send_sms = task.LoopingCall(self._submit_sm)
-            self.send_sms.start(0.01)
-        elif str(pdu.status) == 'ESME_RINVSYSID':
-            print 'Invalid System ID'
+            self.state = self.BINDED
+            self._send_sms.start(0.2)
+        elif pdu.commandId.key == 'unbind_resp' and pdu.status.key == 'ESME_ROK':
+            self.transport.loseConnection()
+            print 'Unbinding done'
+        else:
+            print 'Get pdu {0}'.format(pdu)
 
     def connectionMade(self):
-        self.state = 'BINDING'
+        self.state = self.CONNECTED
         self._bind()
 
     def connectionLost(self, reason):
         print 'onConnectionLost:'
         print self.transport.realAddress, '\n'
-        if self.state == 'UNBINDING':
-            print 'Unbinding done'
 
     def _bin2pdu(self, bindata):
         io_pdu = StringIO(bindata)
@@ -43,23 +55,28 @@ class MyProtocol(Protocol):
 
     def _pdu2bin(self, bindata):
         return PDUEncoder().encode(bindata)
-
+    
+    def _get_seq_num(self):
+        return self.submit_sm_count + 1
+    
     def _submit_sm(self):
-        if SMSCOUNT != self.SMSSENDED:
-            seq_num = self.SMSSENDED + 1
+        if SMSCOUNT > self.submit_sm_count:
+            seq_num = self._get_seq_num()
+            
             print 'Submit sm: ', seq_num
-            message_text = "This is MESSAGE!!1" + str(seq_num)
+
             sm_pdu = operations.SubmitSM(
                 seqNum=seq_num,
-                short_message=message_text,
+                short_message="This is MESSAGE!! {0}".format(seq_num),
                 destination_addr='380660803034',
+                #siurce addre
             )
-            pdubin = self._pdu2bin(sm_pdu)
-            self.transport.write(pdubin)
-            self.SMSSENDED = seq_num
-        else:
-            self.send_sms.stop()
 
+            self.transport.write(self._pdu2bin(sm_pdu))
+            self.submit_sm_count += 1
+        else:
+            self._send_sms.stop()
+        
     def _enqire_link(self):
         print 'Sending enqirelink start\n'
         enqlink = operations.EnquireLink(
@@ -83,8 +100,8 @@ class MyProtocol(Protocol):
                                               system_id=LOGIN,
                                               password=PASSWORD,
                                               system_type='speedflow')
-        pdubin = self._pdu2bin(bind_pdu)
-        self.transport.write(pdubin)
+
+        self.transport.write(self._pdu2bin(bind_pdu))
 
 
 class EchoClientFactory(ClientFactory):
